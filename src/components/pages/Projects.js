c84320d18a79b24eeeb9b7bf42d693c3651a1ead
harika09 <harold.goldensky.com>
2021-06-20 11:29:45 +0800

    improve animation and pricing card

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,7 +6,6 @@ import Project4 from "../images/project4.jpg";
 import Project5 from "../images/project5.jpg";
 import Project6 from "../images/project6.jpg";
 import Project7 from "../images/project7.jpg";
-import Project8 from "../images/project8.jpg";
 
 function Projects() {
   return (
@@ -36,7 +35,7 @@ function Projects() {
             data-aos-easing="ease-in-out"
             data-aos-delay="10"
           >
-            <img src={Project1} alt="Project1" />
+            <img src={Project1} alt="Project" />
             <p>House</p>
           </div>
 
@@ -46,7 +45,7 @@ function Projects() {
             data-aos-easing="ease-in-out"
             data-aos-delay="10"
           >
-            <img src={Project2} alt="Project1" />
+            <img src={Project2} alt="Project" />
             <p>House</p>
           </div>
 
@@ -56,7 +55,7 @@ function Projects() {
             data-aos-easing="ease-in-out"
             data-aos-delay="10"
           >
-            <img src={Project4} alt="Project1" />
+            <img src={Project4} alt="Project" />
             <p>House</p>
           </div>
 
@@ -66,7 +65,7 @@ function Projects() {
             data-aos-easing="ease-in-out"
             data-aos-delay="10"
           >
-            <img src={Project5} alt="Project1" />
+            <img src={Project5} alt="Project" />
             <p>House</p>
           </div>
 
@@ -76,27 +75,17 @@ function Projects() {
             data-aos-easing="ease-in-out"
             data-aos-delay="10"
           >
-            <img src={Project6} alt="Project1" />
+            <img src={Project6} alt="Project" />
             <p>House</p>
           </div>
 
           <div
             className="card-image"
-            data-aos="flip-down"
+            data-aos="fade-up"
             data-aos-easing="ease-in-out"
             data-aos-delay="10"
           >
-            <img src={Project7} alt="Project1" />
-            <p>House</p>
-          </div>
-
-          <div
-            className="card-image"
-            data-aos="flip-left"
-            data-aos-easing="ease-in-out"
-            data-aos-delay="10"
-          >
-            <img src={Project8} alt="Project1" />
+            <img src={Project7} alt="Project" />
             <p>House</p>
           </div>
         </div>
